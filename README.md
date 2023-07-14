b4a2400eece1730f6fa9475b01d69d4933b1d62b
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-07-14 16:22:07 +0900

    스프링부트 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# more-shell
-Spring boot Shell 스크립트를 추가하여 생산성을 올려보자고!
+# spring-boot-shell
+Laravel에서 cli 형식으로 사용하는 artisan 명령어처럼 생산성을 높이는 모든 것들을 Spring boot Shell 스크립트로 구현해서 Spring으로도 cli의 생산성을 느껴보자고! <br/>
+
+## Service Class 만들기
+To be continue...
+
---
+++ src/main/java/kim/mingyu/javacommand/JavaCommandApplication.java
@@ -0,0 +1,13 @@
+package kim.mingyu.javacommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCommandApplication.class, args);
+    }
+
+}
